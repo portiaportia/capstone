7d5a44a5413fedc5e4b1caa030797eb30af4b0c3
portiaportia <portiaplante@gmail.com>
2014-05-21 20:13:26 -0700

    styling

--- settings.php
+++ settings.php
@@ -29,24 +29,18 @@
 	<div id="privacy">
 	    <a id="privacy-toggle" href="#privacy-addition">
 	    	<div class="linebreak">
-		    <img class="icon" src="images/privacy.png"/>
-		    <h3>Privacy</h3>
+		    <h2>Privacy</h2>
 		    </div>
 		    <div id="privacy-desc">
 		    	<p class="settings-desc">
                     Information gathered through this application will not be used 
                     for anything other than commuting purposes.  
-                
-                	<span id="privacy-addition">
-                        The data collected through this application will be summarized 
-                        in terms of miles, and transportation modes at a week level.  
-                        Time of arrival and departure will not be tracked.  Actual route 
-                        details pertaining to your commute will not be kept or viewable by your company.
-					</span>
+                    The data collected through this application will be summarized 
+                    in terms of miles, and transportation modes at a week level.  
+                    Time of arrival and departure will not be tracked.  Actual route 
+                    details pertaining to your commute will not be kept or viewable by your company.
 		    	</p>
 	    	</div>
-				<img class="section-arrow down-arrow-privacy" src="images/section-arrow-down.png">
-		        <img class="section-arrow up-arrow-privacy" src="images/section-arrow-up.png">
 	    </a>
 	</div>
 </section>
@@ -55,8 +49,7 @@
 
 <section class="settings-section ">
 	<div id="auto-track-toggle">
-		<img class="icon" src="images/tracking.png"/>
-		<h3>Automatic Tracking</h3>
+		<h2>Automatic Tracking</h2>
 		<input id="track-check" type="checkbox" checked="checked" />
 	    <script src="js/jquery.onoff.min.js"></script>
 		<script>
@@ -66,19 +59,11 @@
 	    </script>
 		<script src="js/settings.js"></script>
 	</div>
-</section>
-<section class="settings-section">
-	<a id="tracking-toggle" href="#tracking-detail">
-        <p class="settings-desc">
-            Luum uses automatic tracking to report your commute details. 
-            <span id="tracking-detail">
-                You will need to manually enter your commute if you choose to turn this feature off. 
-                Time spent commuting will only be available when automatic tracking is enabled. 
-            </span>
-        </p>
-        <img class="section-arrow down-arrow-tracking" src="images/section-arrow-down.png">
-        <img class="section-arrow up-arrow-tracking" src="images/section-arrow-up.png">
-     </a>
+	<p class="settings-desc">
+        Luum uses automatic tracking to report your commute details. 
+        You will need to manually enter your commute if you choose to turn this feature off. 
+        Time spent commuting will only be available when automatic tracking is enabled. 
+    </p>
 </section>
 	
 </section>
